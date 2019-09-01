3997e6bf778b3aa2754d61f4d5dc51e14558275f
Anubhav Mittal <anubhav11march@gmail.com>
2019-09-01 19:47:10 +0530

    :racehorse: configured backend user database

--- app/src/main/java/com/example/trippy/NameActivity.java
+++ app/src/main/java/com/example/trippy/NameActivity.java
@@ -7,11 +7,15 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NameActivity extends AppCompatActivity {
     private String number;
@@ -28,10 +32,11 @@ public class NameActivity extends AppCompatActivity {
         user = (TextView) findViewById(R.id.user);
         Bundle pnumber = getIntent().getExtras();
         mAuth = FirebaseAuth.getInstance();
-        firebaseUser = mAuth.getCurrentUser();
-//        mRef = database.getReference("Users");
-        name = (EditText) findViewById(R.id.name);
         database = FirebaseDatabase.getInstance();
+        firebaseUser = mAuth.getCurrentUser();
+        mRef = database.getReference().child("Users");
+        name = (EditText) findViewById(R.id.name);
+
         if(pnumber!=null){
             number = pnumber.getString("phoneNumber");
             user.setText("Hi " + number);
@@ -45,7 +50,13 @@ public class NameActivity extends AppCompatActivity {
     public void finni(View view){
         String userName = name.getText().toString().trim();
         if(!TextUtils.isEmpty(userName)){
-
+//            String id = mRef.push().getKey();
+            SimpleDateFormat sdf = new SimpleDateFormat("dd MM yyyy, hh:mm:ss a");
+            String timestamp = sdf.format(new Date());
+            mRef.child(firebaseUser.getUid()).child("Name").setValue(userName);
+            mRef.child(firebaseUser.getUid()).child("Number or Email").setValue(number);
+            mRef.child(firebaseUser.getUid()).child("Last Login").setValue(timestamp);
+            Toast.makeText(this, "Added to "+ firebaseUser.getUid(), Toast.LENGTH_SHORT).show();
         }
     }
 }
